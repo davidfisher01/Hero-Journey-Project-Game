51ef64dcee08d84963352beb3e9466721191402d
007da <007da@DESKTOP-4AP72FA>
2021-05-27 08:42:21 -0700

    Checklist class initiated

---
+++ Hero'sJourney/src/island/Checklist.java
@@ -0,0 +1,16 @@
+package island;
+
+import java.util.ArrayList;
+
+public class Checklist {
+	
+	ArrayList<String> tasklist;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public Checklist() {
+		tasklist = new ArrayList<>();
+	}
+
+}
