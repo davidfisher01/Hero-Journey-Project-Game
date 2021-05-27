6705a8aa52423c39243534b47c7ff3c9358b1fdf
007da <007da@DESKTOP-4AP72FA>
2021-05-27 09:25:21 -0700

    Setup for Checklist and Task Class

--- Hero'sJourney/src/island/Checklist.java
+++ Hero'sJourney/src/island/Checklist.java
@@ -1,16 +1,42 @@
 package island;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class Checklist {
 	
-	ArrayList<String> tasklist;
+	protected ArrayList<Task> tasklist;
+	private Image img;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 	public Checklist() {
 		tasklist = new ArrayList<>();
+		Task catchFish = new Task();
+		Task buildBridge = new Task();
+		Task save = new Task();
+		Task collectFlowers = new Task();
+		Task getToTown = new Task();
+		tasklist.add(catchFish);
+		tasklist.add(buildBridge);
+		tasklist.add(save);
+		tasklist.add(collectFlowers);
+		tasklist.add(getToTown);
+		
+		img = getImage("clipboard.png");
+	}
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Sprite.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
 	}
 
 }
---
+++ Hero'sJourney/src/island/Task.java
@@ -0,0 +1,26 @@
+package island;
+
+public class Task {
+	protected boolean completed;
+	public Task() {
+		completed = false;
+	}
+
+	public void catchFish() {
+		
+	}
+	public void buildBridge() {
+		
+	}
+	public void collectFlowers() {
+		
+	}
+	public void save() {
+		
+	}
+	public void getToTown() {
+		
+	}
+	
+	
+}
