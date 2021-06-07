51e6fd4194475e5fab025654550239229ca6afe0
Francis Fernandez <fosb.promo@gmail.com>
2021-06-07 08:57:48 -0700

    moved collision rectangles out of island and into islandbackground

--- Hero'sJourney/src/island/Island.java
+++ Hero'sJourney/src/island/Island.java
@@ -32,8 +32,6 @@ public class Island extends JPanel implements ActionListener, KeyListener, Mouse
 	Extra e1;
 	Fisherman fisherman;
 	
-	ArrayList<ColRects> iCol = new ArrayList<ColRects>();
-	
 	Text fishermanText;
 	
 	CollectFlowers test;
@@ -103,8 +101,8 @@ public class Island extends JPanel implements ActionListener, KeyListener, Mouse
 		
 		//paint player last
 		g.setColor(Color.black);
-		for(int i = 0; i < iCol.size(); i++) {
-			g.drawRect(iCol.get(i).getX(), iCol.get(i).getY(), iCol.get(i).getWidth(), iCol.get(i).getHeight());
+		for(int k = 0; k < i.getColSize(); k++) {
+			g.drawRect(i.iCol.get(k).getX(), i.iCol.get(k).getY(), i.iCol.get(k).getWidth(), i.iCol.get(k).getHeight());
 			//System.out.println("" + iCol.get(i).getX() + ", " + iCol.get(i).getY() + ", " + iCol.get(i).getWidth() + ", " + iCol.get(i).getHeight());
 		}
 		
@@ -120,8 +118,8 @@ public class Island extends JPanel implements ActionListener, KeyListener, Mouse
 		fisherman.setVy(vy);
 		
 		//collision
-		for(int i = 0; i < iCol.size(); i++) {
-			p.collisionFalse(iCol.get(i));
+		for(int j = 0; j < i.getColSize(); j++) {
+			p.collisionFalse(i.iCol.get(j));
 		}
 		
 		
@@ -130,8 +128,8 @@ public class Island extends JPanel implements ActionListener, KeyListener, Mouse
 		p.collisionTrue(e1);
 		p.collisionTrue(fisherman);
 		
-		for(int i = 0; i < iCol.size(); i++) {
-			p.collisionTrue(iCol.get(i));
+		for(int j = 0; j < i.getColSize(); j++) {
+			p.collisionTrue(i.iCol.get(j));
 		}
 		
 		
@@ -226,8 +224,8 @@ public class Island extends JPanel implements ActionListener, KeyListener, Mouse
 	public void updateBackground() {
 		x += vx;
 		y += vy;
-		for(int i = 0; i < iCol.size();i++) {
-			iCol.get(i).updatePosition(vx, vy);
+		for(int j = 0; j < i.getColSize(); j++) {
+			i.iCol.get(j).updatePosition(vx, vy);
 		}
 		currX -= vx;
 		currY -= vy;
@@ -325,88 +323,6 @@ public class Island extends JPanel implements ActionListener, KeyListener, Mouse
 		
 		currX = midX-50;
 		currY = midY-50;
-		
-		
-		
-		//trees
-		iCol.add(new ColRects(464, -131, 250, 200));
-		iCol.add(new ColRects(1344, 34, 250, 200));
-		iCol.add(new ColRects(779, -491, 250, 200));
-		iCol.add(new ColRects(-246, 49, 250, 200));
-		iCol.add(new ColRects(1414, -476, 250, 200));
-		iCol.add(new ColRects(1299, -906, 250, 200));
-		iCol.add(new ColRects(779, -1041, 250, 200));
-		iCol.add(new ColRects(1509, -2541, 250, 200));
-		iCol.add(new ColRects(-1546, -2476, 250, 200));
-		iCol.add(new ColRects(-1751, -2611, 250, 200));
-		iCol.add(new ColRects(-1826, -2261, 250, 200));
-		iCol.add(new ColRects(-2056, -2361, 250, 200));
-		iCol.add(new ColRects(-2321, -2371, 250, 200));
-		iCol.add(new ColRects(-2581, -2371, 250, 200));
-		iCol.add(new ColRects(-2841, -2506, 250, 200));
-		iCol.add(new ColRects(-3401, -2276, 250, 200));
-		iCol.add(new ColRects(-456, -1051, 250, 200));
-		iCol.add(new ColRects(-1126, -826, 250, 200));
-		iCol.add(new ColRects(-2791, -621, 250, 200));
-		iCol.add(new ColRects(-1806, -181, 250, 200));
-		iCol.add(new ColRects(-1341, -2131, 250, 200));
-		
-		//beach tree
-		iCol.add(new ColRects(-1651, 249, 140, 125));
-		iCol.add(new ColRects(-2836, 114, 140, 125));
-		
-		
-		//houses
-		iCol.add(new ColRects(-2806, -1081, 555, 280));
-		iCol.add(new ColRects(-2796, -1671, 530, 425));
-		iCol.add(new ColRects(-2231, -1846, 535, 605));
-		iCol.add(new ColRects(-1441, -1846, 320, 590));
-		iCol.add(new ColRects(-3216, -3176, 1690, 520));
-		iCol.add(new ColRects(-2831, -3316, 930, 800));
-		iCol.add(new ColRects(619, -1601, 535, 395));
-		
-		//cliff
-		iCol.add(new ColRects(-721, 294, 1075, 210));
-		iCol.add(new ColRects(374, 304, 675, 95));
-		iCol.add(new ColRects(1269, 299, 465, 90));
-		
-		//water
-		iCol.add(new ColRects(-3061, 604, 3580, 80));
-		iCol.add(new ColRects(519, 619, 0, 65));
-		iCol.add(new ColRects(564, 669, 845, 60));
-		iCol.add(new ColRects(1414, 589, 95, 195));
-		iCol.add(new ColRects(1464, 559, 135, 175));
-		iCol.add(new ColRects(1504, 529, 150, 185));
-		iCol.add(new ColRects(1549, 499, 480, 180));
-		iCol.add(new ColRects(1934, 374, 125, 300));
-		iCol.add(new ColRects(1989, -2706, 90, 3290));
-		iCol.add(new ColRects(-41, -2886, 2080, 280));
-		iCol.add(new ColRects(-126, -2861, 190, 185));
-		iCol.add(new ColRects(-336, -2981, 260, 200));
-		iCol.add(new ColRects(-501, -3076, 310, 195));
-		iCol.add(new ColRects(-646, -3191, 380, 200));
-		iCol.add(new ColRects(-786, -3306, 385, 205));
-		iCol.add(new ColRects(-901, -3476, 370, 305));
-		iCol.add(new ColRects(-3726, -3471, 2645, 155));
-		iCol.add(new ColRects(-3906, -3381, 260, 1560));
-		iCol.add(new ColRects(-3781, -1946, 765, 200));
-		iCol.add(new ColRects(-3176, -1946, 155, 725));
-		iCol.add(new ColRects(-3421, -1386, 350, 165));
-		iCol.add(new ColRects(-3541, -1321, 215, 380));
-		iCol.add(new ColRects(-3496, -1056, 480, 120));
-		iCol.add(new ColRects(-3166, -1066, 160, 1820));
-		iCol.add(new ColRects(-696, -771, 45, 15));
-		iCol.add(new ColRects(-1116, -446, 210, 930));
-		iCol.add(new ColRects(-906, -441, 1200, 145));
-		iCol.add(new ColRects(79, -1081, 215, 740));
-		iCol.add(new ColRects(69, -1486, 220, 280));
-		iCol.add(new ColRects(-446, -2056, 905, 550));
-		iCol.add(new ColRects(479, -1911, 180, 190));
-		iCol.add(new ColRects(624, -2056, 540, 425));
-		
-		
-		
-		
 	}
 	
 	public void updateVar() {
--- Hero'sJourney/src/island/IslandBackground.java
+++ Hero'sJourney/src/island/IslandBackground.java
@@ -1,7 +1,10 @@
 package island;
 
+import java.util.ArrayList;
+
 public class IslandBackground extends Sprite{
 	private int vx, vy;
+	public ArrayList<ColRects> iCol = new ArrayList<ColRects>();
 
 	public IslandBackground(String fileName, int width, int height) {
 		super(fileName, width, height);
@@ -9,6 +12,82 @@ public class IslandBackground extends Sprite{
 		vy = -4200;
 		this.width = width;
 		this.height = height;
+		
+		//trees
+		iCol.add(new ColRects(464, -131, 250, 200));
+		iCol.add(new ColRects(1344, 34, 250, 200));
+		iCol.add(new ColRects(779, -491, 250, 200));
+		iCol.add(new ColRects(-246, 49, 250, 200));
+		iCol.add(new ColRects(1414, -476, 250, 200));
+		iCol.add(new ColRects(1299, -906, 250, 200));
+		iCol.add(new ColRects(779, -1041, 250, 200));
+		iCol.add(new ColRects(1509, -2541, 250, 200));
+		iCol.add(new ColRects(-1546, -2476, 250, 200));
+		iCol.add(new ColRects(-1751, -2611, 250, 200));
+		iCol.add(new ColRects(-1826, -2261, 250, 200));
+		iCol.add(new ColRects(-2056, -2361, 250, 200));
+		iCol.add(new ColRects(-2321, -2371, 250, 200));
+		iCol.add(new ColRects(-2581, -2371, 250, 200));
+		iCol.add(new ColRects(-2841, -2506, 250, 200));
+		iCol.add(new ColRects(-3401, -2276, 250, 200));
+		iCol.add(new ColRects(-456, -1051, 250, 200));
+		iCol.add(new ColRects(-1126, -826, 250, 200));
+		iCol.add(new ColRects(-2791, -621, 250, 200));
+		iCol.add(new ColRects(-1806, -181, 250, 200));
+		iCol.add(new ColRects(-1341, -2131, 250, 200));
+		
+		//beach tree
+		iCol.add(new ColRects(-1651, 249, 140, 125));
+		iCol.add(new ColRects(-2836, 114, 140, 125));
+		
+		
+		//houses
+		iCol.add(new ColRects(-2806, -1081, 555, 280));
+		iCol.add(new ColRects(-2796, -1671, 530, 425));
+		iCol.add(new ColRects(-2231, -1846, 535, 605));
+		iCol.add(new ColRects(-1441, -1846, 320, 590));
+		iCol.add(new ColRects(-3216, -3176, 1690, 520));
+		iCol.add(new ColRects(-2831, -3316, 930, 800));
+		iCol.add(new ColRects(619, -1601, 535, 395));
+		
+		//cliff
+		iCol.add(new ColRects(-721, 294, 1075, 210));
+		iCol.add(new ColRects(374, 304, 675, 95));
+		iCol.add(new ColRects(1269, 299, 465, 90));
+		
+		//water
+		iCol.add(new ColRects(-3061, 604, 3580, 80));
+		iCol.add(new ColRects(519, 619, 0, 65));
+		iCol.add(new ColRects(564, 669, 845, 60));
+		iCol.add(new ColRects(1414, 589, 95, 195));
+		iCol.add(new ColRects(1464, 559, 135, 175));
+		iCol.add(new ColRects(1504, 529, 150, 185));
+		iCol.add(new ColRects(1549, 499, 480, 180));
+		iCol.add(new ColRects(1934, 374, 125, 300));
+		iCol.add(new ColRects(1989, -2706, 90, 3290));
+		iCol.add(new ColRects(-41, -2886, 2080, 280));
+		iCol.add(new ColRects(-126, -2861, 190, 185));
+		iCol.add(new ColRects(-336, -2981, 260, 200));
+		iCol.add(new ColRects(-501, -3076, 310, 195));
+		iCol.add(new ColRects(-646, -3191, 380, 200));
+		iCol.add(new ColRects(-786, -3306, 385, 205));
+		iCol.add(new ColRects(-901, -3476, 370, 305));
+		iCol.add(new ColRects(-3726, -3471, 2645, 155));
+		iCol.add(new ColRects(-3906, -3381, 260, 1560));
+		iCol.add(new ColRects(-3781, -1946, 765, 200));
+		iCol.add(new ColRects(-3176, -1946, 155, 725));
+		iCol.add(new ColRects(-3421, -1386, 350, 165));
+		iCol.add(new ColRects(-3541, -1321, 215, 380));
+		iCol.add(new ColRects(-3496, -1056, 480, 120));
+		iCol.add(new ColRects(-3166, -1066, 160, 1820));
+		iCol.add(new ColRects(-696, -771, 45, 15));
+		iCol.add(new ColRects(-1116, -446, 210, 930));
+		iCol.add(new ColRects(-906, -441, 1200, 145));
+		iCol.add(new ColRects(79, -1081, 215, 740));
+		iCol.add(new ColRects(69, -1486, 220, 280));
+		iCol.add(new ColRects(-446, -2056, 905, 550));
+		iCol.add(new ColRects(479, -1911, 180, 190));
+		iCol.add(new ColRects(624, -2056, 540, 425));
 	}
 	
 	public void update() {
@@ -32,6 +111,10 @@ public class IslandBackground extends Sprite{
 
 	public void setVy(int vy) {
 		this.vy = vy;
+	}
+	
+	public int getColSize() {
+		return iCol.size();
 	}
 	
 }
